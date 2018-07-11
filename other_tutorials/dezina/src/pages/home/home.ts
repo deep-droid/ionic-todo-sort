e44286d5fbdbf8061c5f16bf4572f9924d83d418
PK <the.hanging.gardens@gmail.com>
2018-07-11 17:15:47 +0200

    [upd] other tutorials

---
+++ other_tutorials/dezina/src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+import { ProductProvider } from "../../providers/product/product";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public allProducts = [];
+
+  constructor(private productService: ProductProvider, navCtrl: NavController, private http: Http) {
+
+  }
+
+  ionViewDidLoad()
+  {
+    // this.http.get('/assets/data.json')
+    // .map(response => response.json())
+    // .subscribe(data => console.log(data));
+
+    this.productService.getProducts()
+    .subscribe((response) => {
+      this.allProducts = response;
+    });
+  }
+}
