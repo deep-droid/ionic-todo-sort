2bfeb4dd02eb951dcec9a8fb5c76cc9606c481c9
PK <the.hanging.gardens@gmail.com>
2018-10-03 15:54:25 +0200

    [upd] timer update

--- other_tutorials/timerApplication/src/pages/home/home.ts
+++ other_tutorials/timerApplication/src/pages/home/home.ts
@@ -7,8 +7,70 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public myTimer = 60;
+
+  public myTimerVariable;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  public timerController()
+  {
+
+  }
+
+  public customTimer = function()
+    { 
+      this.myTimer--;
+      if (this.myTimer == 0)
+      setTimeout.ca
+      alert("Hello");
+    };
+
+  public startTimer()
+  {
+    setTimeout( this.customTimer, 3000);
+  }
+
+  public stopTimer()
+  {
+
+  }
+
+
+  maxTime: any=3;
+  timerStarted: any = false;
+  timerValue: any = 0;
+  timer: any = 0;
+  hidevalue = false;
+
+  StartTimer(){
+    if (this.timerStarted == false)
+    {
+      this.timerValue = this.maxTime;
+      this.timerStarted = true;  
+    }
+    this.timer = setTimeout(x => 
+      {
+          if(this.timerValue <= 0) { }
+           this.timerValue -= 1;
+
+          if(this.timerValue > 0){
+            //this.hidevalue = false;
+            this.StartTimer();
+          }
+          
+          else{
+              //this.hidevalue = true;
+              this.timerStarted = false;
+              // this.timerValue = 0;
+              alert("Hello");
+              this.timerValue = this.maxTime;
+          }
+
+      }, 1000);
+ 
+
+  }  
 }
