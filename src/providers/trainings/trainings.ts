7b44d779c77e55887482c831b0542a683d15e701
PK <the.hanging.gardens@gmail.com>
2018-10-10 14:30:16 +0200

    [upd] Storage fixed?

--- src/providers/trainings/trainings.ts
+++ src/providers/trainings/trainings.ts
@@ -1,6 +1,6 @@
 // import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage';
 import { TrainingDataModel } from '../../models/trainingdata-model';
 import { reorderArray } from 'ionic-angular';
 
